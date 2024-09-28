a077b677f3809cb90e4dde23018dc177d1f7ca9a
YerosenHunduma <yerosenhunduma@gmail.com>
2024-09-29 02:09:54 +0300

    basic react typescript setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { AppRoutes } from './AppRoutes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  return <>Hello world</>;
+    return (
+        <>
+            <AppRoutes />
+            <ToastContainer autoClose={3000} />
+        </>
+    );
 }
 
 export default App;
